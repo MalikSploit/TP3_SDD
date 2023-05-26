de466214564e98b53864260ddd1361ff1b6bf8ce
yvlanglois <yvlanglois@ada.local.isima.fr>
2023-05-26 15:09:04 +0200

    commentaires eltPile.c

--- TP3_Arbres_2023/eltPile.c
+++ TP3_Arbres_2023/eltPile.c
@@ -11,6 +11,15 @@
  * @param [in] src l'adresse de l'element a copier
  * @param [in] dest l'adresse de la destination
  */
+/* -------------------------------------------------------------------- */
+/* copyElt Copie la valeur d'un element dans un autre                   */
+/*                                                                      */
+/* Cette fonction permet de copier la valeur d'un element dans un autre */
+/* emplacement                                                          */
+/*                                                                      */
+/* En entree:   src adresse de l'element a copier                       */
+/*              dest adresse de l'element de destination                */
+/* -------------------------------------------------------------------- */
 void copyElt(eltType_pile *src, eltType_pile *dest)
 {
     if (src != NULL && dest != NULL)
