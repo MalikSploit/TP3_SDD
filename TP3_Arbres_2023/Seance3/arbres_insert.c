9718979a6b28157aeb6b72dc41212bb73ef76e35
MalikSploit <maksucce@gmail.com>
2023-05-31 09:14:32 +0200

    Seance3

--- TP3_Arbres_2023/Seance3/arbres_insert.c
+++ TP3_Arbres_2023/Seance3/arbres_insert.c
@@ -8,19 +8,70 @@
 #include "../pile.h"
 #include "../eltsArbre.h"
 #include "arbres_insert.h"
+#include "../Seance1/arbres_construct.h"
 
 
 /**
  * @brief rechercher un point de valeur v
- * @param [in] racine pointeur vers la racine de l'arborescence 
- * @return 
+ * @param [in] racine pointeur vers la racine de l'arborescence
+ * @param [in] v la valeur a rechercher
+ * @return
  *   - l'adresse du point contenant v si v existe dans l'arborescence
  *   - NULL, sinon
  */
-//  rechercher_v()
-// {
-// // TO DO
-// }
+cell_lvlh_t* rechercher_v(cell_lvlh_t* racine, char v)
+{
+    cell_lvlh_t* resultat = NULL;
+
+    if (racine != NULL)
+    {
+        pile_t* pile = initPile(NB_ELTPREF_MAX);
+
+        if (pile != NULL)
+        {
+            eltType_pile cour;
+            cour.adrCell = racine;
+            int code;
+
+            // Parcours de l'arborescence tant que le pointeur courant n'est pas NULL et que la pile n'est pas vide
+            while (cour.adrCell != NULL || !estVidePile(pile))
+            {
+                // Parcours en profondeur vers le bas (liens verticaux)
+                while (cour.adrCell != NULL)
+                {
+                    // Si la valeur du point courant correspond à la valeur recherchée, on met à jour le résultat et on arrête le parcours
+                    if (cour.adrCell->val == v)
+                    {
+                        resultat = cour.adrCell;
+                        break;
+                    }
+
+                    // Empilement du point courant pour le parcours ultérieur vers le bas
+                    empiler(pile, &cour, &code);
+                    cour.adrCell = cour.adrCell->lv;
+                }
+
+                // Si le résultat a été trouvé, on arrête le parcours
+                if (resultat != NULL)
+                {
+                    break;
+                }
+
+                // Si la pile n'est pas vide, on dépile un point et on se déplace vers la droite (liens horizontaux)
+                if (!estVidePile(pile))
+                {
+                    depiler(pile, &cour, &code);
+                    cour.adrCell = cour.adrCell->lh;
+                }
+            }
+
+            libererPile(&pile);
+        }
+    }
+
+    return resultat;
+}
+
 
 /**
  * @brief rechercher le double prec de w dans une liste de fils
@@ -28,19 +79,54 @@
  * @param [in] w la valeur a inserer
  * @return l'adresse du pointeur prec apres lequel w doit etre inseree
  */
-//  rechercherPrecFilsTries()
-// {
-// // TO DO
-// }
+cell_lvlh_t** rechercherPrecFilsTries(cell_lvlh_t* adrPere, char w)
+{
+    cell_lvlh_t** pprec = &(adrPere->lv);
 
-/** TO DO
+    // Parcourir les fils jusqu'à trouver le pointeur précédant lequel insérer w
+    while (*pprec != NULL && (*pprec)->val < w) 
+    {
+        pprec = &((*pprec)->lh);
+    }
+
+    return pprec;
+}
+
+
+/**
  * @brief inserer une valeur w dans les fils d'un point de valeur v
  * @param [in] racine la racine de l'arborescence
  * @param [in] v la valeur d'un point auquel on va inserer la valeur w en fils
  * @param [in] w la valeur a inserer
  * @return 1 - insertion realisee; 0 - insertion n'a pas ete realisee
  */
-//  insererTrie()
-// {
-// // TO DO
-// }
+int insererTrie(cell_lvlh_t* racine, char v, char w)
+{
+    cell_lvlh_t* pere = rechercher_v(racine, v);
+    int resultat = 0;
+
+    if (pere != NULL)
+    {
+        cell_lvlh_t** pprec = rechercherPrecFilsTries(pere, w);
+        if (*(pprec) != NULL)
+        {
+            printf("(*pprec)->val = %c\n", (*pprec)->val);
+        }
+
+        // Vérifier si la valeur w n'existe pas déjà dans les fils
+        if (*pprec == NULL || (*pprec)->val != w)
+        {
+            // Allouer un nouveau point de valeur w
+            cell_lvlh_t* nouv = allocPoint(w);
+
+            if (nouv != NULL)
+            {
+                nouv->lh = *pprec;
+                *pprec = nouv;
+                resultat = 1;
+            }
+        }
+    }
+
+    return resultat;
+}
--- TP3_Arbres_2023/Seance3/arbres_insert_main.c
+++ TP3_Arbres_2023/Seance3/arbres_insert_main.c
@@ -26,7 +26,7 @@ TEST(nouvCell) {
 	free(new);
 }
 
-/*
+
 TEST(rechercher_v) {
 	int nbRacines = 0;
 	int nbEltsPref = 0;
@@ -47,41 +47,252 @@ TEST(rechercher_v) {
 	REQUIRE( NULL != pere );
 	CHECK( 'A' == pere->val );
 
-// autres tests a ajouter
+    // Test avec toutes les valeurs existantes dans l'arborescence
+    pere = rechercher_v(racine, 'B');
+    REQUIRE(NULL != pere);
+    CHECK('B' == pere->val);
 
-	libererArbre(&racine);
+    pere = rechercher_v(racine, 'C');
+    REQUIRE(NULL != pere);
+    CHECK('C' == pere->val);
+
+    pere = rechercher_v(racine, 'D');
+    REQUIRE(NULL != pere);
+    CHECK('D' == pere->val);
+
+    pere = rechercher_v(racine, 'E');
+    REQUIRE(NULL != pere);
+    CHECK('E' == pere->val);
+
+    pere = rechercher_v(racine, 'F');
+    REQUIRE(NULL != pere);
+    CHECK('F' == pere->val);
+
+    pere = rechercher_v(racine, 'G');
+    REQUIRE(NULL != pere);
+    CHECK('G' == pere->val);
+
+    pere = rechercher_v(racine, 'H');
+    REQUIRE(NULL != pere);
+    CHECK('H' == pere->val);
+
+    pere = rechercher_v(racine, 'I');
+    REQUIRE(NULL != pere);
+    CHECK('I' == pere->val);
+
+    pere = rechercher_v(racine, 'J');
+    REQUIRE(NULL != pere);
+    CHECK('J' == pere->val);
+
+    pere = rechercher_v(racine, 'K');
+    REQUIRE(NULL != pere);
+    CHECK('K' == pere->val);
+
+    pere = rechercher_v(racine, 'M');
+    REQUIRE(NULL != pere);
+    CHECK('M' == pere->val);
+
+    pere = rechercher_v(racine, 'T');
+    REQUIRE(NULL != pere);
+    CHECK('T' == pere->val);
+
+    libererArbre(&racine);
 }
+
 
 TEST(rechercherPrecFilsTries) {
-	int nbRacines = 0;
-	int nbEltsPref = 0;
-	eltPrefPostFixee_t tabEltPref[NB_ELTPREF_MAX];
-	cell_lvlh_t *racine = NULL;
-	cell_lvlh_t *pere = NULL;
-	cell_lvlh_t **pprec = NULL;
+    int nbRacines = 0;
+    int nbEltsPref = 0;
+    eltPrefPostFixee_t tabEltPref[NB_ELTPREF_MAX];
+    cell_lvlh_t *racine = NULL;
+    cell_lvlh_t *pere = NULL;
+    cell_lvlh_t **pprec = NULL;
 
-	printf("\033[34m\nrechercherPrecFilsTries :");
-	printf("\033[0m\n");
-	nbRacines = lirePref_fromFileName("../pref_exTP.txt", tabEltPref, &nbEltsPref);
-	racine = pref2lvlh(tabEltPref, nbRacines);
+    printf("\033[34m\nrechercherPrecFilsTries :\033[0m\n");
 
-	pere = rechercher_v(racine, 'F');
-	REQUIRE( NULL != pere );
-	CHECK( 'F' == pere->val );
+    nbRacines = lirePref_fromFileName("../pref_exTP.txt", tabEltPref, &nbEltsPref);
+    racine = pref2lvlh(tabEltPref, nbRacines);
 
-	pprec = rechercherPrecFilsTries(pere, 'A');
-	REQUIRE( NULL != *pprec );
-	CHECK( 'K' == (*pprec)->val );
+    // Recherche pour les lettres de A à T
+    pere = rechercher_v(racine, 'F');
+    REQUIRE(NULL != pere);
+    CHECK('F' == pere->val);
 
-// autres tests a ajouter
+    pprec = rechercherPrecFilsTries(pere, 'A');
+    REQUIRE(NULL != *pprec);
+    CHECK('K' == (*pprec)->val);
 
-	libererArbre(&racine);
+    pprec = rechercherPrecFilsTries(pere, 'H');
+    REQUIRE(NULL != *pprec);
+    CHECK('K' == (*pprec)->val);
+
+    pere = rechercher_v(racine, 'B');
+    REQUIRE(NULL != pere);
+    CHECK('B' == pere->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'C');
+    REQUIRE(NULL != *pprec);
+    CHECK('E' == (*pprec)->val);
+
+    pere = rechercher_v(racine, 'C');
+    REQUIRE(NULL != pere);
+    CHECK('C' == pere->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'D');
+    REQUIRE(NULL != *pprec);
+    CHECK('F' == (*pprec)->val);
+
+    pere = rechercher_v(racine, 'A');
+    REQUIRE(NULL != pere);
+    CHECK('A' == pere->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'E');
+    REQUIRE(NULL != *pprec);
+    CHECK('H' == (*pprec)->val);
+
+    pere = rechercher_v(racine, 'C');
+    REQUIRE(NULL != pere);
+    CHECK('C' == pere->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'F');
+    REQUIRE(NULL != *pprec);
+    CHECK('F' == (*pprec)->val);
+
+    pere = rechercher_v(racine, 'F');
+    REQUIRE(NULL != pere);
+    CHECK('F' == pere->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'G');
+    REQUIRE(NULL != *pprec);
+    CHECK('K' == (*pprec)->val);
+
+    pere = rechercher_v(racine, 'F');
+    REQUIRE(NULL != pere);
+    CHECK('F' == pere->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'H');
+    REQUIRE(NULL != *pprec);
+    CHECK('K' == (*pprec)->val);
+
+    pere = rechercher_v(racine, 'C');
+    REQUIRE(NULL != pere);
+    CHECK('C' == pere->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'I');
+    REQUIRE(NULL != *pprec);
+    CHECK('I' == (*pprec)->val);
+
+    pere = rechercher_v(racine, 'B');
+    REQUIRE(NULL != pere);
+    CHECK('B' == pere->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'J');
+    REQUIRE(NULL != *pprec);
+    CHECK('J' == (*pprec)->val);
+
+    pere = rechercher_v(racine, 'F');
+    REQUIRE(NULL != pere);
+    CHECK('F' == pere->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'K');
+    REQUIRE(NULL != *pprec);
+    CHECK('K' == (*pprec)->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'L');
+    REQUIRE(NULL != *pprec);
+    CHECK('M' == (*pprec)->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'M');
+    REQUIRE(NULL != *pprec);
+    CHECK('M' == (*pprec)->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'N');
+    REQUIRE(NULL != *pprec);
+    CHECK('T' == (*pprec)->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'O');
+    REQUIRE(NULL != *pprec);
+    CHECK('T' == (*pprec)->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'P');
+    REQUIRE(NULL != *pprec);
+    CHECK('T' == (*pprec)->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'Q');
+    REQUIRE(NULL != *pprec);
+    CHECK('T' == (*pprec)->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'R');
+    REQUIRE(NULL != *pprec);
+    CHECK('T' == (*pprec)->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'S');
+    REQUIRE(NULL != *pprec);
+    CHECK('T' == (*pprec)->val);
+
+    pprec = rechercherPrecFilsTries(pere, 'T');
+    REQUIRE(NULL != *pprec);
+    CHECK('T' == (*pprec)->val);
+
+
+    // Recherche pour les lettres de U à Z (doivent tous êtres NULL)
+    pprec = rechercherPrecFilsTries(pere, 'U');
+    REQUIRE(NULL == *pprec);
+
+    pprec = rechercherPrecFilsTries(pere, 'V');
+    REQUIRE(NULL == *pprec);
+
+    pprec = rechercherPrecFilsTries(pere, 'W');
+    REQUIRE(NULL == *pprec);
+
+    pprec = rechercherPrecFilsTries(pere, 'X');
+    REQUIRE(NULL == *pprec);
+
+    pprec = rechercherPrecFilsTries(pere, 'Y');
+    REQUIRE(NULL == *pprec);
+
+    pprec = rechercherPrecFilsTries(pere, 'Z');
+    REQUIRE(NULL == *pprec);
+
+    libererArbre(&racine);
 }
+
 
 TEST(insererTrie) {
-// TO DO
+    int nbRacines = 0;
+    int nbEltsPref = 0;
+    eltPrefPostFixee_t tabEltPref[NB_ELTPREF_MAX];
+    cell_lvlh_t *racine = NULL;
+
+    printf("\033[34m\ninsererTrie :\033[0m\n");
+    nbRacines = lirePref_fromFileName("../pref_exTP.txt", tabEltPref, &nbEltsPref);
+    racine = pref2lvlh(tabEltPref, nbRacines);
+
+        // Insérer une valeur en doublon
+        int resultat1 = insererTrie(racine, 'C', 'F');
+        CHECK(0 == resultat1);
+
+        // Insérer une nouvelle valeur
+        int resultat2 = insererTrie(racine, 'F', 'N');
+        CHECK(1 == resultat2);
+        cell_lvlh_t* nouv = rechercher_v(racine, 'N');
+        REQUIRE(NULL != nouv);
+        CHECK('N' == nouv->val);
+        cell_lvlh_t* frere_gauche = nouv->lh;
+        //printf("nouv->lh->val = %c\n", nouv->lh->val);
+        REQUIRE(NULL != frere_gauche);
+        CHECK(frere_gauche->val == 'T');
+        cell_lvlh_t *fils = nouv->lv;
+        CHECK(NULL == fils);
+
+        // Insérer une valeur dans une cellule inexistante
+        int resultat3 = insererTrie(racine, 'X', 'Y');
+        CHECK(0 == resultat3);
+
+        libererArbre(&racine);
 }
-*/
+
+
 END_TEST_GROUP(ARBRE_INSERT)
 
 int main(void) {
